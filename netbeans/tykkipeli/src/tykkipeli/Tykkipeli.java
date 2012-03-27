7c49bca3c3bd1f9d3153c7d814c7fceec2473b76
V.K <kranewitt.cs@gmail.com>
2012-03-27 21:23:48 +0300

    Lisattiin dokumentaatio ja netbeans projektin pohja

---
+++ netbeans/tykkipeli/src/tykkipeli/Tykkipeli.java
@@ -0,0 +1,15 @@
+package tykkipeli;
+
+/**
+ *
+ * 
+ */
+public class Tykkipeli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	System.out.println("Tykkipeli!");
+    }
+}
