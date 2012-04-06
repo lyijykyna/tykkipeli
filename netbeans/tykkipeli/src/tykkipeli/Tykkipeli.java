1d0c3df4afbeb548d5eee2cf5782664819514851
V.K <kranewitt.cs@gmail.com>
2012-04-06 14:46:07 +0300

    Pelaaja-luokka Javadocin kanssa lisätty

--- netbeans/tykkipeli/src/tykkipeli/Tykkipeli.java
+++ netbeans/tykkipeli/src/tykkipeli/Tykkipeli.java
@@ -3,6 +3,8 @@ package tykkipeli;
 import java.awt.Point;
 import tykkipeli.gui.GraafinenKayttoliittyma;
 import tykkipeli.model.Ammus;
+import tykkipeli.model.Pelaaja;
+import tykkipeli.model.Tykki;
 
 /**
  *
@@ -14,6 +16,15 @@ public class Tykkipeli {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+	
+	//pelaajan testausta
+	Tykki tykki = new Tykki();
+	Pelaaja pelaaja = new Pelaaja("Pelaaja1");
+	Pelaaja pelaaja2 = new Pelaaja("Pelaaja2");
+	pelaaja2.setTykki(tykki);
+	pelaaja.ammu();
+	pelaaja2.ammu();
+	
 	GraafinenKayttoliittyma gui = new GraafinenKayttoliittyma();
 	gui.setSize(640, 480);
     }
